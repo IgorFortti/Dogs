469c3a62ef07de167a97cb1e7f0951519096f23b
Igor Fortti <igormfortti@gmail.com>
2023-09-27 10:17:35 -0300

    Initial Commit

---
+++ Dogs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dogs
+//
+//  Created by Igor Fortti on 27/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Igor Fortti on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
